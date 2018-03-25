c697a6c99f2c0209e284c2d38f13d54d8e21c2c6
art-defcon <john@petroff.org>
2018-03-25 14:23:39 +0200

    Starting file for Arduino Project

---
+++ arduino_wemos_esp32/arduino_wemos_esp32.ino
@@ -0,0 +1,15 @@
+/*
+  Arduino Project for WeMoS LOLIN, ESP32/ESP-WROOM-32 Board
+*/
+
+#include <WiFi.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
